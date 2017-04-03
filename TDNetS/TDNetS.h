cc240dcf99dacc6da1ac4dc7fc77872bd0e0d9f1
adrianj <adrian.johnson@chelsea-apps.com>
2017-04-03 12:31:13 +0100

    Initial Commit

---
+++ TDNetS/TDNetS.h
@@ -0,0 +1,19 @@
+//
+//  TDNetS.h
+//  TDNetS
+//
+//  Created by Adrian Johnson on 03/04/2017.
+//  Copyright © 2017 Training Dragon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDNetS.
+FOUNDATION_EXPORT double TDNetSVersionNumber;
+
+//! Project version string for TDNetS.
+FOUNDATION_EXPORT const unsigned char TDNetSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDNetS/PublicHeader.h>
+
+
